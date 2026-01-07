4acf8ff5bd5e7f4d39110209c6e02d1b9590d154
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-07 03:45:24 +0000

    Added templates for movies app

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mystery/", include("movies.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
