a16093aa47590a1449818d813cc8312fc38a7552
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-07 00:33:13 +0000

    Added admin config to movies app

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import MysteryTitle
 
-# Register your models here.
+
+@admin.register(MysteryTitle)
+class MysteryTitleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "release_year",
+        "media_type",
+        "avg_quality",
+        "fair_play_consensus",
+    )
+    list_filter = ("media_type", "is_fair_play_candidate")
+    search_fields = ("title", "director")
+    prepopulated_fields = {"slug": ("title",)}
+
+    fieldsets = (
+        (None, {"fields": ("title", "slug", "media_type", "release_year", "director")}),
+        ("Synopsis", {"fields": ("description",)}),
+        (
+            "Mystery Metrics",
+            {
+                "fields": (
+                    "is_fair_play_candidate",
+                    "avg_quality",
+                    "avg_difficulty",
+                    "fair_play_consensus",
+                )
+            },
+        ),
+    )
