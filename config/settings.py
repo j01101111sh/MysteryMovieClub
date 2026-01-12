a312e2a6bc627771094af5dea0f7937edfc6c878
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-12 20:42:15 +0000

    Moved heatmap CSS to app-specific CSS

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Define where you will put your source static files (e.g., global css/js)
