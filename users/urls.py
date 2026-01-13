c0a1ceea9ff8496e123ee8953407146fc8e7ad40
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-13 22:31:59 +0000

    Added basic user profile feature

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, UserDetailView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/<str:username>/", UserDetailView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,30 @@
+from typing import Any
+
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")  # Assuming 'login' URL will exist
     template_name = "registration/signup.html"
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "users/user_detail.html"
+    context_object_name = "profile_user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = self.object.review_set.select_related("movie").order_by(
+            "-created_at"
+        )
+        return context
