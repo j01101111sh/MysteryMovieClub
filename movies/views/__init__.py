f5245cb08332c795fcbd8860c49d72932ee930c1
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-14 22:55:19 +0000

    sorted alphabetically

--- movies/views/__init__.py
+++ movies/views/__init__.py
@@ -9,13 +9,13 @@ from .taxonomy import (
 from .titles import MysteryDetailView, MysteryListView
 
 __all__ = [
-    "TagVoteView",
-    "ReviewCreateView",
-    "ReviewListView",
     "DirectorDetailView",
     "DirectorListView",
-    "SeriesDetailView",
-    "SeriesListView",
     "MysteryDetailView",
     "MysteryListView",
+    "ReviewCreateView",
+    "ReviewListView",
+    "SeriesDetailView",
+    "SeriesListView",
+    "TagVoteView",
 ]
