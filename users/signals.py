8a92b4f4e9a1c47b477e274b022e7f5976c84c4c
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-14 19:56:52 +0000

    Cleaned up signal

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Any
 
-from django.db.models.base import ModelBase
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -12,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=CustomUser)
 def log_user_creation(
-    sender: ModelBase,
+    sender: type[CustomUser],
     instance: CustomUser,
     created: bool,
     **kwargs: Any,
