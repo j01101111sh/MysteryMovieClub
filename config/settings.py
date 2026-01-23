cb559172a89db97f5bdc0d15a45f0d3c5c95f457
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-23 06:03:30 +0000

    Moved logs to logs folder

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ LOGGING = {
         "file": {
             "level": "DEBUG",
             "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": BASE_DIR / "django.log",
+            "filename": BASE_DIR / "logs/django.log",
             "formatter": "verbose",
             "when": "H",
             "interval": os.getenv("LOG_BACKUP_INTERVAL_HOURS", 1),
